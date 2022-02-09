da3d40916bc8d3711b468b1d63302aa1d0ba6fd8
mlinds <max.lindsay95@gmail.com>
2022-02-09 13:52:29 +0100

    Update update_spot.bat

--- update_spot.bat
+++ update_spot.bat
@@ -2,5 +2,5 @@ C:\Users\maxli\anaconda3\envs\spotify\python.exe D:\Projects\spotify-data\downlo
 git config user.name "Automated"
 git config user.email "actions@users.noreply.github.com"
 git add -A
-git commit -m "Latest data: %date%%time%" || exit 0
+git commit -m "Latest data: %date% %time%" || exit 0
 git push
