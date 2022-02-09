4ca4b8bed37bcacefb18ba4e22945fbd8dcf6a23
Automated <actions@users.noreply.github.com>
2022-02-09 20:12:56 +0100

    Latest data: Wed 09 Feb 2022 20:12:56.79

--- update_spot.bat
+++ update_spot.bat
@@ -1,7 +1,6 @@
 C:\Users\maxli\anaconda3\envs\spotify\python.exe D:\Projects\spotify-data\download_functions.py
-git config --worktree
-git config user.name "Automated"
-git config user.email "actions@users.noreply.github.com"
+git config --worktree user.name "Automated"
+git config --worktree user.email "actions@users.noreply.github.com"
 git add -A
 git commit -m "Latest data: %date% %time%" || exit 0
 git push
