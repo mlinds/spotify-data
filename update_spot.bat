748d1cf291b32f687d08d3f61ab3298b9b25d9a3
mlinds <max.lindsay95@gmail.com>
2022-02-09 20:16:41 +0100

    testing how to seperate commits

--- update_spot.bat
+++ update_spot.bat
@@ -1,9 +1,9 @@
 C:\Users\maxli\anaconda3\envs\spotify\python.exe D:\Projects\spotify-data\download_functions.py
-git config --worktree user.name "Automated"
-git config --worktree user.email "actions@users.noreply.github.com"
-git add -A
+git config user.name "Automated"
+git config user.email "actions@users.noreply.github.com"
+git add .\data_out\*
 git commit -m "Latest data: %date% %time%" || exit 0
 git push
-@REM git config user.name "mlinds"
-@REM git config user.email "max.lindsay95@gmail.com"
-@REM git push
+git config user.name "mlinds"
+git config user.email "max.lindsay95@gmail.com"
+git push
